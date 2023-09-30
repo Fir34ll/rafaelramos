13640cfd028051c64ce3626d6b3bd54465fbe5ec
Rafael Ramos <rafaelfilhoramos@hotmail.com>
2023-09-29 21:43:32 -0300

    Comit Arquivo Binario

---
+++ binario.py
@@ -0,0 +1,25 @@
+import pickle
+def criar_contato():
+    name = input("Nome: ")
+    phone_number = input("Número de telefone: ")
+    email = input("E-mail: ")
+    contato = {
+        "name": name,
+        "phone_number": phone_number,
+        "email": email
+    }
+    return contato
+def salvar_contatos(contatos):
+    with open("contatos.bin", "wb") as f:
+        pickle.dump(contatos, f)
+def main():
+    contatos = []
+    while True:
+        contato = criar_contato()
+        contatos.append(contato)
+        choice = input("Deseja continuar? (S/N) ")
+        if choice == "N":
+            break
+    salvar_contatos(contatos)
+if __name__ == "__main__":
+    main()
